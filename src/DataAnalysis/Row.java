141876b8c4a1f4e52a95bd1abcaa9ba284501b0d
Antonio <atomas@elpuig.xeill.net>
2021-01-20 20:02:18 +0100

    Añadido lombok y creado package para Data Analysis

---
+++ src/DataAnalysis/Row.java
@@ -0,0 +1,18 @@
+package DataAnalysis;
+
+import lombok.Data;
+
+@Data
+public class Row {
+    private String n_m_registre_taller;
+    private String nom_titular;
+    private String r_tol;
+    private String adre_a_taller;
+    private String codi_postal;
+    private String municipi;
+    private String municipi_codi_idescat;
+    private String provincia;
+    private String tel_fon;
+    private String estat;
+    private String data_alta;
+}

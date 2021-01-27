7d78488d293e5ec6fbe2c5d0e38a78701ea406c3
Antonio <atomas@elpuig.xeill.net>
2021-01-27 18:37:41 +0100

    Comienzo de menú

--- src/DataAnalysis/Menu.java
+++ src/DataAnalysis/Menu.java
@@ -7,5 +7,12 @@ public class Menu {
     Scanner scanner = new Scanner(System.in);
     private List<Row> rows;
 
+    public Menu(List<Row> rows) {
+        this.rows = rows;
+    }
 
+    public void showMenu(){
+        System.out.println("--- Red de talleres de Catalunya ---");
+        System.out.println("");
+    }
 }
